ab2a27b6d3b6484603b707b0d3bd970f3af00835
Daniel Khan <danielkhan@car-booth-15-mac.content.linkedin.biz>
2019-11-04 09:36:40 -0800

    video finish

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (request, response) => {
+    response.send('Hello Express :)');
+});
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}!`);
+});
