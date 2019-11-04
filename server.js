5be639388693fe570572fb503e58e38d253861ee
Daniel Khan <danielkhan@car-booth-15-mac.content.linkedin.biz>
2019-11-04 10:48:52 -0800

    02_03 final

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (request, response) => {
-    response.send('Hello Express :)');
+    response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response) => {
+    response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
