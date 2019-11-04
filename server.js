2011b778d0576c5046b35a5b6dc28da75618820f
Daniel Khan <danielkhan@car-booth-15-mac.content.linkedin.biz>
2019-11-04 11:56:00 -0800

    prettier and eslint

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, './static/index.html'));
+  response.sendFile(path.join(__dirname, './static/index.html'));
 });
 
 app.get('/speakers', (request, response) => {
-    response.sendFile(path.join(__dirname, './static/speakers.html'));
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Express server listening on port ${port}!`);
+  console.log(`Express server listening on port ${port}!`);
 });
