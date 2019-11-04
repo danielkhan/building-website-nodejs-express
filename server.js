e44b274cdbae41d74b384d36170e8b2c53e3c9ab
Daniel Khan <danielkhan@car-booth-15-mac.content.linkedin.biz>
2019-11-04 13:34:06 -0800

    add ejs

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (request, response) => {
