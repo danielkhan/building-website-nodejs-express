5606e0641e01124923ce152a24c122c410c6d4e5
Daniel Khan <danielkhan@car-booth-15-mac.content.linkedin.biz>
2019-11-04 16:37:43 -0800

    add sessions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
 
 const FeedbackService = require('./services/FeedbackService');
 const SpeakersService = require('./services/SpeakerService');
@@ -12,6 +13,15 @@ const routes = require('./routes');
 const app = express();
 
 const port = 3000;
+
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['Ghdur687399s7w', 'hhjjdf89s866799'],
+  })
+);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
