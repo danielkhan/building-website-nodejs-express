87927dcf19edb63651c89592a28a4f57db785b64
Daniel Khan <danielkhan@car-booth-15-mac.content.linkedin.biz>
2019-11-05 12:39:17 -0800

    template variables

--- server.js
+++ server.js
@@ -26,7 +26,20 @@ app.use(
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
+app.locals.siteName = 'ROUX Meetups';
+
 app.use(express.static(path.join(__dirname, './static')));
+
+app.use(async (request, response, next) => {
+  try {
+    const names = await speakersService.getNames();
+    response.locals.speakerNames = names;
+    console.log(response.locals);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
 
 app.use(
   '/',
