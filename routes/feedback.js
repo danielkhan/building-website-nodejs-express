8cfb50627f84d33dae95e58e3c6e3a02bed67b88
Daniel Khan <danielkhan@car-booth-15-mac.content.linkedin.biz>
2019-11-06 14:20:48 -0800

    storing data

--- routes/feedback.js
+++ routes/feedback.js
@@ -12,6 +12,9 @@ module.exports = params => {
       const feedback = await feedbackService.getList();
 
       const errors = request.session.feedback ? request.session.feedback.errors : false;
+
+      const successMessage = request.session.feedback ? request.session.feedback.message : false;
+
       request.session.feedback = {};
 
       return response.render('layout', {
@@ -19,6 +22,7 @@ module.exports = params => {
         template: 'feedback',
         feedback,
         errors,
+        successMessage,
       });
     } catch (err) {
       return next(err);
@@ -49,7 +53,7 @@ module.exports = params => {
         .escape()
         .withMessage('A message is required'),
     ],
-    (request, response) => {
+    async (request, response) => {
       const errors = validationResult(request);
 
       if (!errors.isEmpty()) {
@@ -58,7 +62,13 @@ module.exports = params => {
         };
         return response.redirect('/feedback');
       }
-      return response.send('Feedback form posted');
+
+      const { name, email, title, message } = request.body;
+      await feedbackService.addEntry(name, email, title, message);
+      request.session.feedback = {
+        message: 'Thank you for your feedback!',
+      };
+      return response.redirect('/feedback');
     }
   );
 
