2725b21c20086a0e320b38432f5bced51b8ec172
Daniel Khan <danielkhan@car-booth-15-mac.content.linkedin.biz>
2019-11-06 14:06:51 -0800

    validating and sanitizing

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { check, validationResult } = require('express-validator');
+
 const router = express.Router();
 
 module.exports = params => {
@@ -8,20 +10,57 @@ module.exports = params => {
   router.get('/', async (request, response, next) => {
     try {
       const feedback = await feedbackService.getList();
+
+      const errors = request.session.feedback ? request.session.feedback.errors : false;
+      request.session.feedback = {};
+
       return response.render('layout', {
         pageTitle: 'Feedback',
         template: 'feedback',
         feedback,
+        errors,
       });
     } catch (err) {
       return next(err);
     }
   });
 
-  router.post('/', (request, response) => {
-    console.log(request.body);
-    return response.send('Feedback form posted');
-  });
+  router.post(
+    '/',
+    [
+      check('name')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A name is required'),
+      check('email')
+        .trim()
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('A valid email address is required'),
+      check('title')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A title is required'),
+      check('message')
+        .trim()
+        .isLength({ min: 5 })
+        .escape()
+        .withMessage('A message is required'),
+    ],
+    (request, response) => {
+      const errors = validationResult(request);
+
+      if (!errors.isEmpty()) {
+        request.session.feedback = {
+          errors: errors.array(),
+        };
+        return response.redirect('/feedback');
+      }
+      return response.send('Feedback form posted');
+    }
+  );
 
   return router;
 };
