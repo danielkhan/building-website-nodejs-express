69ff211e5e5dfb1a8628bde8a37a9009f4a0809c
Daniel Khan <danielkhan@car-booth-15-mac.content.linkedin.biz>
2019-11-06 08:27:43 -0800

    add route

--- routes/speakers.js
+++ routes/speakers.js
@@ -7,7 +7,7 @@ module.exports = params => {
 
   router.get('/', async (request, response) => {
     const speakers = await speakersService.getList();
-    return response.json(speakers);
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
   });
 
   router.get('/:shortname', (request, response) => {
