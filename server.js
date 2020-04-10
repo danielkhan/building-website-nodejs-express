752689b2fa1b17f122bcc204c093a82f695bcdc3
slutske22 <seth.lutske@gmail.com>
2020-04-09 22:31:58 -0700

    getting going

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', (request, response) => {
+  response.render('pages/index', { pageTitle: 'Welcome' });
+});
+
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
