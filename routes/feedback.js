5bce96c0a36fd1008a6c6eceab63780c8829e73f
Seth Lutske <seth.lutske@gmail.com>
2020-04-16 20:24:34 +0000

    finishing up

--- routes/feedback.js
+++ routes/feedback.js
@@ -2,6 +2,31 @@ const express = require('express');
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
+
+const validations =
+   [
+      check('name')
+         .trim()
+         .isLength({ min: 3 })
+         .escape()
+         .withMessage('A name is required'),
+      check('email')
+         .trim()
+         .isEmail()
+         .normalizeEmail()
+         .withMessage('A valid email is required'),
+      check('title')
+         .trim()
+         .isLength({ min: 3 })
+         .escape()
+         .withMessage('A title is required'),
+      check('message')
+         .trim()
+         .isLength({ min: 5 })
+         .escape()
+         .withMessage('A message is required'),
+   ]
+
 module.exports = params => {
    const { feedbackService } = params;
 
@@ -25,48 +50,49 @@ module.exports = params => {
    });
 
    router.post(
-      '/',
-      [
-         check('name')
-            .trim()
-            .isLength({ min: 3 })
-            .escape()
-            .withMessage('A name is required'),
-         check('email')
-            .trim()
-            .isEmail()
-            .normalizeEmail()
-            .withMessage('A valid email is required'),
-         check('title')
-            .trim()
-            .isLength({ min: 3 })
-            .escape()
-            .withMessage('A title is required'),
-         check('message')
-            .trim()
-            .isLength({ min: 5 })
-            .escape()
-            .withMessage('A message is required'),
-      ],
-      async (request, response) => {
-         const errors = validationResult(request);
+      '/', validations,
+      async (request, response, next) => {
 
-         if (!errors.isEmpty()) {
+         try {
+            const errors = validationResult(request);
+
+            if (!errors.isEmpty()) {
+               request.session.feedback = {
+                  errors: errors.array(),
+               };
+               return response.redirect('/feedback');
+            }
+
+            const { name, email, title, message } = request.body;
+            await feedbackService.addEntry(name, email, title, message);
             request.session.feedback = {
-               errors: errors.array(),
+               message: 'Thank you for your feedback.',
             };
+
             return response.redirect('/feedback');
+         } catch (err) {
+            return next(err)
          }
 
-         const { name, email, title, message } = request.body;
-         await feedbackService.addEntry(name, email, title, message);
-         request.session.feedback = {
-            message: 'Thank you for your feedback.',
-         };
 
-         return response.redirect('/feedback');
       }
    );
+
+
+   router.post('/api', validations, async (request, response, next) => {
+      try {
+         const errors = validationResult(request)
+         if (!errors.isEmpty()) {
+            return response.json({ errors: errors.array() })
+         }
+         const { name, email, title, message } = request.body;
+         await feedbackService.addEntry(name, email, title, message);
+         const feedback = await feedbackService.getList()
+         return response.json({ feedback, successMessage: "Thank you for your feedback." })
+      } catch (err) {
+         return next(err)
+      }
+   })
 
    return router;
 };
--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(
 );
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
