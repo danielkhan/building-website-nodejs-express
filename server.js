14d1d845fe67cdda0eecabb539dfcaba1297054e
Mwanje Mike <mwanjemike767@gmail.com>
2020-07-18 17:59:06 +0300

    Lesson 5 complete

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const { request, response } = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (request, response) => {
-    response.send('Hello Express :');
+  response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Express server listening on port ${port}!`);
+  console.log(`Express server listening on port ${port}!`);
 });
