e26450662d9fe697f9b8d4ea4e0cc8bd23fa1026
Mwanje Mike <mwanjemike767@gmail.com>
2020-07-19 12:20:26 +0300

    Added session management with a small visit counter

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,13 @@ const router = express.Router();
 
 module.exports = (params) => {
     router.get('/', (request, response) => {
+        //a small visit counter
+        if(!request.session.visitcount) {
+            request.session.visitcount = 0;            
+        }
+        request.session.visitcount += 1;
+        console.log(`Number of visits: ${request.session.visitcount}`);
+
         response.render('pages/index', { pageTitle: 'Welcome' });
     });
 
