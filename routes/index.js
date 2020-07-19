c6adf5f2078cb01f1df6ea5a1a30797fbbde0cc6
Mwanje Mike <mwanjemike767@gmail.com>
2020-07-19 13:31:52 +0300

    Created a site wide layout

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,7 @@ const router = express.Router();
 
 module.exports = (params) => {
     router.get('/', (request, response) => {
-        //a small visit counter
-        if(!request.session.visitcount) {
-            request.session.visitcount = 0;            
-        }
-        request.session.visitcount += 1;
-        console.log(`Number of visits: ${request.session.visitcount}`);
-
-        response.render('pages/index', { pageTitle: 'Welcome' });
+        response.render('layout', { pageTitle: 'Welcome', template: '' });
     });
 
     router.use('/speakers', speakersRoute(params));
