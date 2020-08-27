a499fd523180d292935157a18a264a5e1fa84e1a
Elizabeth Him <elizabethxhim@gmail.com>
2020-08-27 05:17:12 -0700

    Update speakers detail page

--- routes/speakers.js
+++ routes/speakers.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-module.exports = params => {
+module.exports = (params) => {
   const { speakersService } = params;
 
   router.get('/', async (request, response) => {
@@ -10,8 +10,10 @@ module.exports = params => {
     response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
   });
 
-  router.get('/:shortname', (request, response) => {
-    return response.send(`Detail page of ${request.params.shortname}`);
+  router.get('/:shortname', async (request, response) => {
+    const speaker = await speakersService.getSpeaker(request.params.shortname);
+    console.log(speaker);
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers-detail', speaker });
   });
 
   return router;
