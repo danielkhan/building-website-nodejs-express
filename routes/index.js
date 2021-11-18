c899176164243ae8f72be384b79e9b9f8d40d049
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-18 11:04:44 -0800

    export module

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
-
 const router = express.Router()
 
-// routes HTTP GET requests. takes path and callback as args
-router.get('/', (req, res) => {
-  // renders view from pages/index. defines local pageTitle variable of 'Welcome' for the view
-  res.render('pages/index', {pageTitle: 'Welcome'})
-});
+module.exports = () => {
+  // routes HTTP GET requests. takes path and callback as args
+  router.get('/', (req, res) => {
+    // renders view from pages/index. defines local pageTitle variable of 'Welcome' for the view
+    res.render('pages/index', {pageTitle: 'Welcome'})
+  });
+
+  return router
+}
