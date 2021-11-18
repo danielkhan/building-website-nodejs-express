a835fe2721fac49a9d96bc5a00330f4b0b0c8ec9
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-18 06:16:46 -0800

    serving html pages and static content

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const express = ("express")
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/index.html'))
+})
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}!`)
+})
