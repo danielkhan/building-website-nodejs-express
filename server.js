112a8f1c8ed584df25d118f27dfbe54566bb612f
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-18 09:30:48 -0800

    configured eslint and prettier

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-const port = 3000
+const port = 3000;
+app.use(express.static(path.join('./static')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/index.html'))
-})
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
 
 app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'))
-})
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Express server listening on port ${port}!`)
-})
+  console.log(`Express server listening on port ${port}!`);
+});
