43641967b00f596b61c205fa416b76d364c981ca
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-18 09:43:30 -0800

    installed and configured ejs template engine

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require("path");
 const app = express();
 
 const port = 3000;
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './views'))
 app.use(express.static(path.join('./static')));
 
 app.get('/', (req, res) => {
