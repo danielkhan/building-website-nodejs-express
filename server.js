cd0224d601af1587c4aea8ccfff4107e465c9f3b
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-18 10:40:39 -0800

    added comments for clarity

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const path = require("path");
 
 const app = express();
 
+// sets port to 3000
 const port = 3000;
-
+// sets ejs to be the value of view engine
 app.set('view engine', 'ejs')
+// sets path to views directory as the value of views
 app.set('views', path.join(__dirname, './views'))
 
+// app.use mounts specified middleware function at the specifie path, function is executed when base of requested path matches path
+// express.static serves static files, arg specifies root directory from which to serve static assets from.
 app.use(express.static(path.join('./static')));
 
+// routes HTTP GET requests. takes path and callback as args
 app.get('/', (req, res) => {
   res.render('pages/index', {pageTitle: 'Welcome'})
 });
@@ -18,6 +23,7 @@ app.get('/speakers', (req, res) => {
   res.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
+// binds and listens for connections on specified port
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
 });
